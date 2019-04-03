4127083cc29e195a4f0975402487decd183b3922
Murray Jack <mjack@starrez.com>
2019-04-03 20:18:43 +1100

    lastests

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,28 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './assets/IMG_0198.JPG';
 import './App.css';
 
+const styles = {
+    position: "fixed",
+    top: "0",
+    left: "0",
+    width: "100vw",
+    height: "100vh",
+    objectFit: "cover",
+}
+
+const labels = {
+    position: "fixed",
+    top: "0",
+    left: "0",
+    width: "100vw",
+    height: "100vh",
+    backgroundColor: "rgba(0, 0, 0, 0.8)",
+    color: "white",
+}
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <img style={styles} src={logo} />
+                <div style={labels} >
+                    <h1>Coming Soon</h1>
+                    <p>Alan Knipe: Innovation Day 2019</p>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
